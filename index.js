464abd26d75dc32d6c940104df7253aa4615b2a2
Luke Chen <luke.chen.2010@gmail.com>
2020-07-18 23:09:10 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
 // Code your solutions in this file
-function writeCards( names, event ) {
+function writeCards( names, event ) 
+{
   let cards = []
-  for ( let i = 0; i < names.length; i++ ) {
+  for ( let i = 0; i < names.length; i++ ) 
+  {
     cards.push( `Thank you, ${names[i]}, for the wonderful ${event} gift!` )
   }
   return thankYouCards
 }
 
-function countDown( startingNumber ) {
-  while ( startingNumber > 0 ) {
-    console.log( startingNumber );
-    startingNumber -= 1;
+function countDown( start ) 
+{
+  while ( start > 0 ) 
+  {
+    console.log( start );
+    start -= 1;
   }
   console.log( startingNumber );
 }
